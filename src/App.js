9f0191e251ca44082dc0007d0db489134e7343e5
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-12-06 03:50:35 +0600

    login logout system added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { createContext, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home/Home";
+import Personal from "./components/Registration/Personal/Personal";
+import Registration from "./components/Registration/Registration/Registration";
 
 export const UserContext = createContext();
 
@@ -9,6 +11,7 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
 
   return (
+<<<<<<< HEAD
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
       <Router>
         <Switch>
@@ -21,6 +24,21 @@ function App() {
         </Switch>
       </Router>
     </UserContext.Provider>
+=======
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/registration">
+          <Registration></Registration>
+        </Route>
+      </Switch>
+    </Router>
+>>>>>>> 75777e050cf0f8f2e5774f6e4301196177fd6731
   );
 }
 
