179e89538d2bc8cb15b22dd687ffae113a20b757
Md. Sakif Uddin Khan <sakif@dojoit.com>
2020-12-21 21:24:46 +0600

    Fixing Match List Card

--- src/components/UserDashboard/NewMatchListCard/NewMatchListCard.js
+++ src/components/UserDashboard/NewMatchListCard/NewMatchListCard.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Moment from 'react-moment';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
 
 const NewMatchListCard = ({ name, dateOfBirth, score, height, address }) => {
   console.log(dateOfBirth);
@@ -12,28 +12,28 @@ const NewMatchListCard = ({ name, dateOfBirth, score, height, address }) => {
 
   return (
     <div
-      style={{ backgroundColor: '#f8f8f8' }}
-      className="row d-flex justify-content-between m-2 px-2 pt-1"
+      style={{ backgroundColor: "#f8f8f8" }}
+      className="row d-flex justify-content-start m-2 px-2 pt-1"
     >
-      <div className="col-md-3 img-fluid ">
+      <div className="col-md-2">
         <img
           style={{ width: 50, borderRadius: 40 }}
-          className="img-fluid mr-1"
+          className="img-fluid mr-1 "
           src="https://i.imgur.com/mtxJUHB.jpg"
           alt=""
           srcSet=""
         />
       </div>
       <Link to="/user/profileMatch">
-        <div className="col-md-9">
-          <p style={{ lineHeight: '100%' }}>
-            <span style={{ fontSize: 14, fontWeight: 'bold' }}>{name}</span>
+        <div className="col-md-7">
+          <p style={{ lineHeight: "100%" }}>
+            <span style={{ fontSize: 14, fontWeight: "bold" }}>{name}</span>
             <br />
             <small>
               <p style={{ fontSize: 11 }}>
                 {date} old | {height} | {address}
                 <br />
-                <small style={{ color: '#8e8be6' }}>{score}% Match</small>
+                <small style={{ color: "#8e8be6" }}>{score}% Match</small>
               </p>
             </small>
           </p>
