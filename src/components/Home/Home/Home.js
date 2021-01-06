9ab255fb055700a94f239791399c544085ddfd02
zabir1998 <zabirahmed1998@gmail.com>
2021-01-07 04:33:16 +0600

    home page done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import CopyRight from "../../CopyRight/CopyRight/CopyRight";
 import Couples from "../Couples/Couples";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import HeaderForm from "../HeaderForm/HeaderForm";
+import Icons from "../Icons/Icons";
+import Membership from "../Membership/Membership";
 import NavBar from "../NavBar/NavBar";
 import StepSection from "../StepSection/StepSection";
+import Subscribe from "../Subscribe/Subscribe";
 
 const Home = () => {
   return (
@@ -14,6 +19,11 @@ const Home = () => {
       <HeaderForm></HeaderForm>
       <StepSection></StepSection>
       <Couples></Couples>
+      <Membership></Membership>
+      <Icons></Icons>
+      <Subscribe></Subscribe>
+      <Footer></Footer>
+      <CopyRight></CopyRight>
     </Container>
   );
 };
